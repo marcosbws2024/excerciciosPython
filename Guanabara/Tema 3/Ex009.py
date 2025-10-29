e80c31f1a9f48ad4906495dd7bde12d33d2fa6ce
marcosbws2024 <marcosbws2024@gmail.com>
2025-10-29 05:55:03 -0300

    novos exercicios

---
+++ Guanabara/Tema 3/Ex009.py
@@ -0,0 +1,2 @@
+metros = float(input("Digite a quantidade de metros: "))
+print("Valor convertido em milimetros: {}".format(metros*1000))
---
+++ Guanabara/Tema 3/Ex015.py
@@ -0,0 +1,5 @@
+salario = float(input("Digite o valor do Salário: "))
+
+abono = (salario * 15) / 100
+novosalario = salario + abono
+print("Salario com Aumento: {}".format(novosalario))
